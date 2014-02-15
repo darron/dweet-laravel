6e8d2e955cd0f4d9fbfeed021f387c77bb34e3da
Darron Froese <darron@froese.org>
2014-02-15 10:13:30 -0700

    Added some basic example tests.

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -13,5 +13,42 @@ class ExampleTest extends TestCase {
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
+  
+  public function testDweetUrl()
+  {
+    $crawler = $this->client->request('GET', '/dweet');
+
+    $this->assertTrue($this->client->getResponse()->isOk());
+  }
+  
+  public function testSpecificDweetAndVars()
+  {
+    $crawler = $this->client->request('GET', '/dweet/for/darron?hi=this&and=that');
+    $this->assertTrue($this->client->getResponse()->isOk());
+  }
+
+  public function testSecondSpecificDweetAndVars()
+  {
+    $crawler = $this->client->request('GET', '/dweet/for/darron?him=these&andy=those');
+    $this->assertTrue($this->client->getResponse()->isOk());
+  }
+
+  public function testSpecificDweet()
+  {
+    $crawler = $this->client->request('GET', '/dweet/for/darron');
+    $this->assertTrue($this->client->getResponse()->isOk());
+  }
+  
+  public function testGetLastDweetForDarron()
+  {
+    $crawler = $this->client->request('GET', '/get/latest/dweet/for/darron');
+    $this->assertTrue($this->client->getResponse()->isOk());
+  }
+  
+  public function testAllDweetsForDarron()
+  {
+    $crawler = $this->client->request('GET', '/get/dweets/for/darron');
+    $this->assertTrue($this->client->getResponse()->isOk());
+  }
 
 }
